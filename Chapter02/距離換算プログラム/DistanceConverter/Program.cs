ce5e6a22b09a043c512e39473d0849207686662e
圓岡　さくら <oibc37211@ojs.ac.jp>
2026-04-22 09:44:56 +0900

    コマンドライン引数の対応

--- Chapter02/距離換算プログラム/DistanceConverter/Program.cs
+++ Chapter02/距離換算プログラム/DistanceConverter/Program.cs
@@ -2,38 +2,57 @@
 
 namespace DistanceConverter
 {
-    internal class Program
+    internal class Progra
     {
         static void Main(string[] args)
         {
 
             if (args.Length >= 1 && args[0] == "-tom")
             {
-                //フィートからメートルへの対応表を出力
-                for (int feet = 1; feet <= 10; feet++)
-                {
-                    double meter = FeetToMeter(feet);
-                    Console.WriteLine($"{feet}ft = {meter:0.0000}m");
-                }
+                PrintFeetToMeterList(1, 10);
+            }
+            else if (args.Length >= 1 && args[0] == "-tof")
+            {
+                PrintMeterToFeetList(1, 10);
+
             }
             else {
-                for(int meter = 1; meter <= 10; meter++)
-                {
-                    double feet = MeterToFeet(meter);
-                    Console.WriteLine($"{meter}ft = {feet:0.0000}m");
-                }
-
+                Console.WriteLine("引数エラー");
             }
+        }
 
+
+
+        static void PrintFeetToMeterList(int start, int stop)
+        {
+
+            //フィートからメートルへの対応表を出力
+            for (int feet = start; feet <= stop; feet++)
+            {
+                double meter = FeetToMeter(feet);
+                Console.WriteLine($"{feet}ft = {meter:0.0000}m");
+            }
+        }
+        static void PrintMeterToFeetList(int start, int stop)
+        {
+            //メートルからフィートの対応表を出力
+            for (int meter = start; meter <= stop; meter++)
+            {
+                double feet = MeterToFeet(meter);
+                Console.WriteLine($"{meter}m = {feet:0.0000}ft");
+            }
         }
         //フィートからメートルを求める
-        static double FeetToMeter(int feet) {
+        static double FeetToMeter(int feet)
+        {
             return feet * 0.3048;
         }
         //メートルからフィートを求める
-        static double MeterToFeet(int meter) {
+        static double MeterToFeet(int meter)
+        {
             return meter / 0.3048;
 
         }
+
     }
 }
