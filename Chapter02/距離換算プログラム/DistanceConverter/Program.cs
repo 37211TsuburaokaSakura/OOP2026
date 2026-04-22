793df087bac713a901ef56c563c567be5f934981
圓岡　さくら <oibc37211@ojs.ac.jp>
2026-04-22 11:46:37 +0900

    修正ver

--- Chapter02/距離換算プログラム/DistanceConverter/Program.cs
+++ Chapter02/距離換算プログラム/DistanceConverter/Program.cs
@@ -4,56 +4,67 @@ namespace DistanceConverter
 {
     internal class Progra
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
 
-            if (args.Length >= 1 && args[0] == "-tom")
-            {
-                PrintFeetToMeterList(int.Parse(args[1]), int.Parse(args[2]));
-                
+            if (args.Length == 3 
+                && int.TryParse(args[1], out int start)
+                && int.TryParse(args[2], out int end)){ 
+
+                if (args[0] == "-tom"){
+                    PrintFeetToMeterList(start, end);
+                }
+                else if(args[0] == "-tof") {
+                    PrintFeetToMeterList(start, end);
+                }
+                else {
+                    Console.WriteLine("引数エラー");
+                }
             }
-            else if (args.Length >= 1 && args[0] == "-tof")
-            {
-                PrintMeterToFeetList(int.Parse(args[1]), int.Parse(args[2]));
 
-            }
-            else {
-                Console.WriteLine("引数エラー");
-            }
-        }
-
-
-
-        static void PrintFeetToMeterList(int start, int stop)
-        {
+            /*
+       if (args.Length >= 1 && args[0] == "-tom"){
+               PrintFeetToMeterList(int.Parse(args[1]), int.Parse(args[2]));
+           }
+           else if (args.Length >= 1 && args[0] == "-tof"){
+               PrintMeterToFeetList(int.Parse(args[1]), int.Parse(args[2]));
+           }
+           else
+           {
+               Console.WriteLine("引数エラー");
+           }
+   }
+*/
 
             //フィートからメートルへの対応表を出力
-            for (int feet = start; feet <= stop; feet++)
+            static void PrintFeetToMeterList(int start, int stop)
             {
-                double meter = FeetToMeter(feet);
-                Console.WriteLine($"{feet}ft = {meter:0.0000}m");
+
+                for (int feet = start; feet <= stop; feet++)
+                {
+                    double meter = FeetToMeter(feet);
+                    Console.WriteLine($"{feet}ft = {meter:0.0000}m");
+                }
+            }
+            static void PrintMeterToFeetList(int start, int stop)
+            {
+                //メートルからフィートの対応表を出力
+                for (int meter = start; meter <= stop; meter++)
+                {
+                    double feet = MeterToFeet(meter);
+                    Console.WriteLine($"{meter}m = {feet:0.0000}ft");
+                }
+            }
+            //フィートからメートルを求める
+            static double FeetToMeter(int feet)
+            {
+                return feet * 0.3048;
+            }
+            //メートルからフィートを求める
+            static double MeterToFeet(int meter)
+            {
+                return meter / 0.3048;
+
             }
         }
-        static void PrintMeterToFeetList(int start, int stop)
-        {
-            //メートルからフィートの対応表を出力
-            for (int meter = start; meter <= stop; meter++)
-            {
-                double feet = MeterToFeet(meter);
-                Console.WriteLine($"{meter}m = {feet:0.0000}ft");
-            }
-        }
-        //フィートからメートルを求める
-        static double FeetToMeter(int feet)
-        {
-            return feet * 0.3048;
-        }
-        //メートルからフィートを求める
-        static double MeterToFeet(int meter)
-        {
-            return meter / 0.3048;
-
-        }
-
     }
 }
---
+++ Chapter04/文字列を数値に変換する/Code4_36/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq.Expressions;
+
+namespace Code4_36
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "1234";
+
+            if (int.TryParse(str, out var height))
+            {
+                Console.WriteLine(height+"：変換成功");
+
+            }
+            else {
+                Console.WriteLine("変換できません");
+            }
+
+            //pardで変換する場合は例外処理が必要となる
+            try {
+                int num = int.Parse(str);
+            }
+            catch (Exception) {
+                Console.WriteLine("変換できません");
+
+            }
+
+            
+        }
+    }
+}
