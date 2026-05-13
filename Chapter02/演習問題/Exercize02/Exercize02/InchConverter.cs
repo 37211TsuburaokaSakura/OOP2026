68bd4370c1877feeb5a48afa2b6ed145673a5cdb
圓岡　さくら <oibc37211@ojs.ac.jp>
2026-05-13 11:34:01 +0900

    inchからmの変換

---
+++ Chapter02/演習問題/Exercize02/Exercize02/InchConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercize02 {
+    internal class InchConverter {
+        private static readonly double ratio = 0.0254;
+
+        //メートルからインチを求める
+        public static double FromMater(double meter) {
+            return meter / ratio;
+        }
+        //インチからメートルを求める
+        public static double ToMeter(double inch) {
+            return inch * ratio;
+        }
+    }
+}
--- Chapter02/演習問題/Exercize02/Exercize02/Program.cs
+++ Chapter02/演習問題/Exercize02/Exercize02/Program.cs
@@ -1,7 +1,16 @@
 ﻿namespace Exercize02 {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            PrintInchToMeterList(1, 10);
+        }
+
+        //
+        static void PrintInchToMeterList(int start, int stop) {
+            for (int inch = start; inch <= stop; inch++) {
+
+                double meter = InchConverter.ToMeter(inch);
+                Console.WriteLine($"{inch}inch = {meter:0.0000}m");
+            }
         }
     }
 }
