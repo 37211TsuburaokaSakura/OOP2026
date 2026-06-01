74e9f3bdc1a96f1e62afa098d4f7002fb1ae0309
圓岡　さくら <oibc37211@ojs.ac.jp>
2026-06-01 10:04:40 +0900

    5.1.1 完成

---
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    //5.1.1
+    public class YearMonth {
+        public int Year { get; set; }
+        public int Month { get; set; }
+
+        //コンストラクタ
+        public YearMonth(int year, int month) {
+            Year = year;
+            Month = month;
+        }
+        
+    }
+}
