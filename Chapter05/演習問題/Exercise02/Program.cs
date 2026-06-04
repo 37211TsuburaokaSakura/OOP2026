c98a50c671df0b07077b43874b9e46c0beec258a
圓岡　さくら <oibc37211@ojs.ac.jp>
2026-06-04 09:38:55 +0900

    2.2.2

--- Chapter05/演習問題/Exercise02/Program.cs
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -14,6 +14,11 @@ namespace Exercise02 {
 
             Console.WriteLine("5.2.2");
             Exercise2(ymCollection);
+            
+            for (int i = 0; i <ymCollection.Length;i++){
+                Console.WriteLine(ymCollection[i]);
+               
+            }
 
             Console.WriteLine("5.2.4");
             Exercise4(ymCollection);
