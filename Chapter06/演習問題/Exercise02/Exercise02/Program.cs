5188a3ea8836e804b70ab9eb7908d8c050eec792
圓岡　さくら <oibc37211@ojs.ac.jp>
2026-06-10 10:38:42 +0900

    6.2

---
+++ Chapter06/演習問題/Exercise02/Exercise02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.WebSockets;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var read = Console.ReadLine();
+
+            if (int.TryParse(read, out var result)) {
+                int num = result;
+                var s1 = num.ToString("#,0");
+                Console.WriteLine(s1);
+
+            } else {
+                Console.WriteLine("数字文字列に変換できません");
+               
+            }
+        }
+    }
+}
