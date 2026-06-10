f90060c1533ccfcdb51e58e1f99057bf1e19f148
圓岡　さくら <oibc37211@ojs.ac.jp>
2026-06-10 09:55:27 +0900

    6.1 完成

---
+++ Chapter06/文字列の比較/Section01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var str1 = "あいうえおが";
+            var str2 = "ｱｲｳｴｵｶﾞ";
+
+            var cultureInfo = new CultureInfo("ja-jp");
+
+            if (String.Compare(str1, str2,cultureInfo,CompareOptions.IgnoreWidth | CompareOptions.IgnoreKanaType)==0) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+
+            }
+        }
+    }
+}
