2592fed04bf28f31dcfc0e311112b32b12fbbcd3
圓岡　さくら <oibc37211@ojs.ac.jp>
2026-06-15 10:35:48 +0900

    カンマ区切り

---
+++ Chapter06/文字列の連結と結合/Section05/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace Section05 {
+    internal class Program {
+        static void Main(string[] args) {
+      //      var sb = new StringBuilder();
+            
+            var kon = ",";
+            var result = string.Join(kon,GetWords());
+            Console.WriteLine(result);
+        }
+        private static IEnumerable<object> GetWords() {
+            return ["Orange","Lemon","Strawberry"];
+        }
+    }
+}
