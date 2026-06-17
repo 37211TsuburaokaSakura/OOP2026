6c51eed9630fec67731a04621127bac097037577
圓岡　さくら <oibc37211@ojs.ac.jp>
2026-06-17 10:19:38 +0900

    3.4

---
+++ Chapter05/演習問題/Exercise04/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection.Metadata;
+
+namespace Exercise04 {
+    internal class Program {
+        static void Main(string[] args) {
+            var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+            var ans = line.Split(';');
+            foreach (var item in ans) {
+                var ans2 = item.Split('=');
+                Console.WriteLine($"{ToJapanese(ans2[0])}:{ans2[1]}" );
+            }
+
+            
+
+
+
+        }
+        static string ToJapanese(string key) {
+            return key switch {
+                "Novelist" => "作家",
+                "BestWork" => "代表作",
+                "Born" => "誕生年",
+                _ => "引数keyは、正しい値ではありません"
+            };
+            //古い書き方
+            //switch (key) {
+            //    case "Novelist":　
+            //        return "作家";
+            //    case "BestWork":
+            //        return "代表作";
+            //    case "Born":
+            //        return "誕生年";
+            //    default:
+            //        return "引数keyは、正しい値ではありません";
+            //}
+        }
+    }
+}
