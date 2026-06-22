a9ae6f7ad9c334463cc89e5efc3b2062ce66d5b6
圓岡　さくら <oibc37211@ojs.ac.jp>
2026-06-22 11:28:04 +0900

    設問１_Studentクラス完成

--- TestProject/Test01_01/Student.cs
+++ TestProject/Test01_01/Student.cs
@@ -1,6 +1,8 @@
 ﻿namespace Test01_01 {
     public class Student {
-        
+        public string Name { get; set; }
+        public string Subject { get; set; }
+        public int Score { get; set; }
 
 
     }
