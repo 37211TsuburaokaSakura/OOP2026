0464acf33bae63d54eb4b3fb6692b1871473f80e
圓岡　さくら <oibc37211@ojs.ac.jp>
2026-06-22 10:39:32 +0900

    7.2.5

--- Chapter07/演習問題/Exercise02/Program.cs
+++ Chapter07/演習問題/Exercise02/Program.cs
@@ -61,7 +61,8 @@ namespace Exercise02 {
         }
 
         private static void Exercise5(List<Book> books) {
-            
+            var num = books.Where(b => b.Price <= 4000).Max(b => b.Pages);
+            Console.WriteLine(num);
         }
 
         private static void Exercise6(List<Book> books) {
