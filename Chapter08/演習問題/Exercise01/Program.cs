79c8e34fce0bb75a014cc3e109c337066c0ed2e8
圓岡　さくら <oibc37211@ojs.ac.jp>
2026-06-29 15:49:35 +0900

    8章　演習問題準備

---
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Cozy Iummox gives smart squid who asks for job pen";
+
+            Exercise01(text);
+            Console.WriteLine();
+            Exercise02(text);
+        }
+
+        private static void Exercise01(string text) {
+           
+        }
+
+        private static void Exercise02(string text) {
+           
+        }
+    }
+}
