a975669a58b9722628b067f7e80723ba7d0a7a36
圓岡　さくら <oibc37211@ojs.ac.jp>
2026-08-24 15:41:57 +0900

    ☆コンボボックスの履歴更新

--- FormApps/CarReportSystem/Form1.cs
+++ FormApps/CarReportSystem/Form1.cs
@@ -286,11 +286,22 @@ namespace CarReportSystem {
                         FileAccess.Read//アクセス
                         )) {
                         listCarReports = (BindingList<CarReport>)bf.Deserialize(fs);
-                        dgvRecords.DataSource = listCarReports;
+                        dgvRecords.DataSource = listCarReports;                       
+                    }
+                    //コンボボックスの履歴をすべて消す
+                    cbAuthor.Items.Clear();
+                    cbCarName.Items.Clear();
 
+                    //コンボボックスの履歴を再登録
+                    foreach (var report in listCarReports) {
+                        SetCbAuthor(report.Author);
+                        SetCbCarName(report.CarName);
                     }
                 }
-                catch (Exception){ }
+                catch (Exception ex){
+                    tsslbMessage.Text = "ファイル書き出しエラー";
+                    MessageBox.Show(ex.Message);
+                }
                 }
         }
     }
