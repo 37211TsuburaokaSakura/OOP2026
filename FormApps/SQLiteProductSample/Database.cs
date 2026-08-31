7f7bbc7bf5b9d7aa533d0a41bc916a3e6f83a82f
圓岡　さくら <oibc37211@ojs.ac.jp>
2026-08-31 14:12:56 +0900

    Datebaseクラス作成

--- FormApps/SQLiteProductSample/Database.cs
+++ FormApps/SQLiteProductSample/Database.cs
@@ -2,7 +2,51 @@ using Microsoft.Data.Sqlite;
 
 namespace SQLiteProductSample;
 
-public static class Database
-{
-  
+//sqlliteデータベースの接続と初期化を担当するクラス
+public static class Database {
+
+    //DBファイルの保存場所
+    private static readonly string DatabasePath = Path.Combine(AppContext.BaseDirectory, "products.db");
+
+    //sqlloiteへ接続するための接続文字列
+    private static readonly string ConnectionString = $"Date source = {DatabasePath}";
+
+    //DBファイルの保存場所を外部から確認するための読み取り専用プロパティ
+    public static string FilePath => DatabasePath;
+
+
+    //新しいsqlliteConnectionを生成して返す
+    public static SqliteConnection GetConnection() {
+        return new SqliteConnection(ConnectionString);
+    }
+
+
+    //DBの初期化処理
+    public static void Initialize() {
+        //接続オブジェクトを生成する
+        using var connection = GetConnection();
+
+        //DBを開く
+        connection.Open();
+
+        //sqlを実行するためのコマンドプロジェクトを作る
+        using var command = connection.CreateCommand();
+
+
+        //productテーブルを作るｓｑｌ
+        //if not exist により、すでにテーブルがあってもエラーにならない
+        command.CommandText =
+            """
+             CREATE TABLE IF NOT EXISTS Products(
+                Id     INTEGER PRIMARY KEY AUTOINCREMENT,
+                Name   TEXT NOT NULL
+                Price  INTEGER NOT NULL CHACK(Price >=0)
+             
+             );
+            """;
+
+        //結果行を返さないsqlを実行する
+        command.ExecuteNonQuery();
+    }
+
 }
